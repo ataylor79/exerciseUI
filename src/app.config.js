9e66830355704fe978eb0f381d045dfc33eb4e47
alec taylor <alec.taylor79@gmail.com>
2016-01-30 18:31:08 +0000

    Getting to grips with ui-routing

--- src/app.config.js
+++ src/app.config.js
@@ -1,6 +1,16 @@
-routing.$inject = ['$urlRouterProvider', '$locationProvider'];
+routing.$inject = ['$urlRouterProvider', '$locationProvider', '$stateProvider'];
 
-export default function routing($urlRouterProvider, $locationProvider) {
+export default function routing($urlRouterProvider, $locationProvider, $stateProvider) {
   $locationProvider.html5Mode(true);
   $urlRouterProvider.otherwise('/');
+
+  $stateProvider
+    .state('progress', {
+      url: '/progress',
+      template: require('./pages/progress.html')
+    })
+    .state('profile', {
+      url: '/profile',
+      template: require('./pages/user.html')
+    });
 }
--- src/features/user/user.routes.js
+++ src/features/user/user.routes.js
@@ -3,9 +3,17 @@ routes.$inject = ['$stateProvider'];
 export default function routes($stateProvider) {
   $stateProvider
     .state('user', {
-      url: '/',
+      url: '/users',
+      template: require('./users.html')
+    })
+    .state('user.list', {
+      url: '/list',
       template: require('./userList.html'),
       controller: 'UserController',
       controllerAs: 'UserController'
+    })
+    .state('user.details', {
+      url: '/details',
+      template: require('./userDetails.html'),
     });
 }
